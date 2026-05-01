9973ebc91179e782ac22de95c722157c160ee45c
zZHOENG <ZHONGZHENGGROUP@163.com>
2026-05-01 18:34:54 +0800

    Change onBrokenLinks behavior from 'throw' to 'warn'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'zZHOENG', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
