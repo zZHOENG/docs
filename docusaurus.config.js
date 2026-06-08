bddcf81d7e909d7c56d4b842def6b2592b48f13d
zZHOENG <ZHONGZHENGGROUP@163.com>
2026-06-08 23:28:35 +0800

    Add remarkFold plugin to Docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,7 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import remarkFold from './src/plugins/remark-fold.mjs';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -35,7 +36,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',   // 我们马上会重写这个文件
-          remarkPlugins: [remarkMath],
+          remarkPlugins: [remarkMath, remarkFold],
           rehypePlugins: [rehypeKatex],
           showLastUpdateTime: false,
         },
