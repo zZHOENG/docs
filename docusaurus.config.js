b6163591d1df04f3438e0ab26c84d40526bce800
zZHOENG <ZHONGZHENGGROUP@163.com>
2026-06-08 23:24:02 +0800

    Remove remark-directive from Docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,6 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-import remarkDirective from 'remark-directive';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -36,7 +35,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',   // 我们马上会重写这个文件
-          remarkPlugins: [remarkMath, remarkDirective],
+          remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
           showLastUpdateTime: false,
         },
